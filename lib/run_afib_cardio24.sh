bf8154317993c8af63b9c8b1e90915b035fea770
saisrisathya <saisrisathya@gmail.com>
2015-10-31 12:25:25 +0530

    modified run_afib shell script to log output on the console

--- lib/run_afib_cardio24.sh
+++ lib/run_afib_cardio24.sh
@@ -1,39 +1,49 @@
 #!/bin/sh
 # script for execution of deployed applications
 #
-# Sets up the MCR environment for the current $ARCH and executes 
+# Sets up the MCR environment for the current $ARCH and executes
 # the specified command.
+# arg1 = matlab runtime
+# arg2 = input filename
+# arg3 = sampling frequency
 #
 exe_name=$0
 exe_dir=`dirname "$0"`
-echo "------------------------------------------"
+#echo "------------------------------------------"
 if [ "x$1" = "x" ]; then
   echo Usage:
   echo    $0 \<deployedMCRroot\> args
 else
-  echo Setting up environment variables
+#  echo Setting up environment variables
   MCRROOT="$1"
-  echo ---
+#  echo ---
   LD_LIBRARY_PATH=.:${MCRROOT}/runtime/glnxa64 ;
   LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/bin/glnxa64 ;
   LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/sys/os/glnxa64;
 	MCRJRE=${MCRROOT}/sys/java/jre/glnxa64/jre/lib/amd64 ;
-	LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRJRE}/native_threads ; 
+	LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRJRE}/native_threads ;
 	LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRJRE}/server ;
 	LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRJRE}/client ;
-	LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRJRE} ;  
+	LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRJRE} ;
   XAPPLRESDIR=${MCRROOT}/X11/app-defaults ;
   export LD_LIBRARY_PATH;
   export XAPPLRESDIR;
-  echo LD_LIBRARY_PATH is ${LD_LIBRARY_PATH};
+#  echo LD_LIBRARY_PATH is ${LD_LIBRARY_PATH};
   shift 1
   args=
   while [ $# -gt 0 ]; do
       token=`echo "$1" | sed 's/ /\\\\ /g'`   # Add blackslash before each blank
-      args="${args} ${token}" 
+      args="${args} ${token}"
       shift
   done
   "${exe_dir}"/afib_cardio24 $args
 fi
-exit
 
+#read output file generated from matlab and echo its contents
+filename=$2
+#cat ${filename::-4}_op.txt
+echo "::startoutput::"
+#cat 11f97c225e3204761f41378e1f2779e7_op.txt
+cat ${filename::-4}_op.txt
+echo "::endoutput::"
+exit
